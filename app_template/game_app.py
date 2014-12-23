3a51b425fad4934e499cf00dccbbad1cc8af4e3f
Igor Kolyadkin <i.kolyadkin@wysegames.com>
2014-12-23 12:06:48 +0200

    comment out engine to application connection layer

--- app_template/game_app.py
+++ app_template/game_app.py
@@ -1,38 +1,39 @@
-from engine import AbstractGameApp
-from engine.user.user_manager import UserManager
+# Uncomment lines below to enable Engine layer to use with Application layer.
 
-# NOTE: registers all commands
-from game_logic.commands import *
-from game_logic.commands.base import GameCommandProcessor
-from game_logic.content_manager import ContentManager
+# from engine import AbstractGameApp
+# from engine.user.user_manager import UserManager
+
+# from game_logic.commands import *
+# from game_logic.commands.base import GameCommandProcessor
+# from game_logic.content_manager import ContentManager
 
 
-class GameApp(AbstractGameApp):
-    name = None
-
-    def get_command_processor_class(self, application_settings):
-        """
-        This one should return Application CommandProcessor class
-
-        :param application_settings:
-        :return:
-        """
-        return GameCommandProcessor
-
-    def get_user_manager(self, application_settings):
-        """
-        This one should return application UserManager instance with application_settings
-
-        :param application_settings:
-        :return:
-        """
-        return UserManager(application_settings)
-
-    def get_content_manager(self, application_settings):
-        """
-        This one should return application ContentManager instance with application_settings
-
-        :param application_settings:
-        :return:
-        """
-        return ContentManager(application_settings)
+# class GameApp(AbstractGameApp):
+#     name = None
+#
+#     def get_command_processor_class(self, application_settings):
+#         """
+#         This one should return Application CommandProcessor class
+#
+#         :param application_settings:
+#         :return:
+#         """
+#         return GameCommandProcessor
+#
+#     def get_user_manager(self, application_settings):
+#         """
+#         This one should return application UserManager instance with application_settings
+#
+#         :param application_settings:
+#         :return:
+#         """
+#         return UserManager(application_settings)
+#
+#     def get_content_manager(self, application_settings):
+#         """
+#         This one should return application ContentManager instance with application_settings
+#
+#         :param application_settings:
+#         :return:
+#         """
+#         return ContentManager(application_settings)
